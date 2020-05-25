0a39f14d55eba7687e6e20f3f0ca7a55ae9e0898
luiscastro8p <luisarmandocastro12@gmail.com>
2020-05-25 03:21:05 -0600

    [UPDATE]Se agrego lista de reportes

--- src/app/routes/report/report.component.ts
+++ src/app/routes/report/report.component.ts
@@ -1,15 +1,73 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { DatatableComponent } from "@swimlane/ngx-datatable";
+import Swal from "sweetalert2";
 
 @Component({
-  selector: 'app-report',
-  templateUrl: './report.component.html',
-  styleUrls: ['./report.component.scss']
+  selector: "app-home",
+  templateUrl: "./report.component.html",
+  styleUrls: ["./report.component.scss"],
 })
 export class ReportComponent implements OnInit {
+  @ViewChild("table") tableExp: any;
+  @ViewChild(DatatableComponent) table: DatatableComponent;
+  filterPost = "";
+  editing = {};
+  listAdmin = [
+    {
+      id: 2323,
+      name:"bache",
+      gravedad: 3,
+      description:"Es un bache bien cabron que lleva varios dias asi",
+      lat: 25.7582117,
+      lng: -108.9833722,
+      phone: 8188080,
+    },
+  ];
+  temp = [];
+  timeout: any;
+  expanded: any = {};
+  selected = [];
+  constructor() {}
+  updateFilter(event) {
+    console.log("entro");
 
-  constructor() { }
+    const val = event.target.value.toLowerCase();
+    // filter our data
+    const temp = this.temp.filter(function (d) {
+      return (
+        d.gravedad.toLowerCase().indexOf(val) !== -1 ||
+        d.description.toLowerCase().indexOf(val) !== -1 ||
+        d.phone.toLowerCase().indexOf(val) !== -1 ||
+        d.name.toLowerCase().indexOf(val) !== -1 ||
+        !val
+      );
+    });
+    console.log(temp);
 
-  ngOnInit() {
+    // update the rows
+    this.listAdmin = temp;
+    // Whenever the filter changes, always go back to the first page
+    this.table.offset = 0;
   }
 
+  ngOnInit() {
+    for (let i = 0; i < 5; i++) {
+      let arr = {
+        id: this.getRandomInt(100, 2),
+        name: "bache" + (i + 1),
+        gravedad: this.getRandomInt(4, 1),
+        description: "Es un bache bien cabron que lleva varios dias asi",
+        lat: 25.7582117,
+        lng: -108.9833722,
+        phone: this.getRandomInt(1000000, 2),
+      };
+      this.listAdmin.push(arr);
+    }
+  }
+
+  getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  delete() {}
 }
--- src/app/routes/report/report.module.ts
+++ src/app/routes/report/report.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { ReportComponent } from './report.component';
 import { Routes, RouterModule } from '@angular/router';
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { CommonModule } from "@angular/common";  
 
 const routes: Routes = [{ path: "", component: ReportComponent }];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes), NgxDatatableModule],
+  imports: [RouterModule.forChild(routes), NgxDatatableModule, CommonModule],
   declarations: [ReportComponent],
   exports: [RouterModule],
-  
 })
 export class ReportModule {}
